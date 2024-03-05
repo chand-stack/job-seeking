2ef5ff396cfb8957082d2112cda86bce044f964c
Chand Rahman <chand.stack@gmail.com>
2024-03-05 02:58:03 -0700

    Chore: navbar added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Nav } from '../../Shared/Nav'
 
 export const Home = () => {
   return (
-    <div>this is home Home</div>
+    <div>
+        <Nav/>
+    </div>
   )
 }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from '../Components/Pages/Home/Home'
+import { Root } from '../MainLayout/Root';
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>,
+      element: <Root/>,
+      children: [{
+        path:"/",
+        element:<Home/>
+      }]
     },
   ]);
