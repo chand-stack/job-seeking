5d4ee4bd09c1a0eef204e56b51634f478dc294db
Chand Rahman <chand.stack@gmail.com>
2024-03-07 04:21:06 -0700

    Chore: companies section added to home page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Nav } from '../../Shared/Nav'
 import { Banner } from '../../Shared/Banner'
 import { Alljobs } from '../../Shared/Alljobs'
+import { Companies } from '../../Shared/Companies'
 
 export const Home = () => {
   return (
     <div>
         
         <Banner/>
+        <Companies/>
         <Alljobs/>
     </div>
   )
---
+++ src/Components/Shared/Companies.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SiVodafone } from "react-icons/si";
+import { SiIntel } from "react-icons/si";
+import { SiTesla } from "react-icons/si";
+import { SiAmd } from "react-icons/si";
+export const Companies = () => {
+  return (
+    <div className='container mx-auto my-20'>
+<h1 className='font-bold text-slate-400 text-start text-2xl'>Companies we helped grow</h1>
+<div className='text-slate-400 flex flex-wrap items-center justify-between'>
+<div className='flex items-center gap-2'>
+    <SiVodafone className='text-6xl'/>
+    <h1 className='text-5xl font-bold'>Vodafone</h1>
+</div>
+<div>
+<SiIntel className='text-9xl'/>
+</div>
+<div className='flex items-center gap-2'>
+<SiTesla className='text-6xl'/>
+    <h1 className='text-5xl font-bold'>Tesla</h1>
+</div>
+<div>
+<SiAmd className='text-9xl'/>
+</div>
+<div>
+<h1 className='text-6xl font-bold'>Talkit</h1> 
+</div>
+</div>
+    </div>
+  )
+}
