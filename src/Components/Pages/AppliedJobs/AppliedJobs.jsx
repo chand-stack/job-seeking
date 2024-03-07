068debe43cbca8584b6b8fc70725a59ea1bf8ff9
Chand Rahman <chand.stack@gmail.com>
2024-03-07 03:45:29 -0700

    Chore: user dashboard added

--- src/Components/Pages/AppliedJobs/AppliedJobs.jsx
+++ src/Components/Pages/AppliedJobs/AppliedJobs.jsx
@@ -1,13 +1,104 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FaBowlingBall } from 'react-icons/fa'
+import { AuthContext } from '../../../AuthProvider/AuthProvider'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export const AppliedJobs = () => {
+const {user} = useContext(AuthContext)
+
+const { data: jobs, isLoading } = useQuery({
+  queryKey: ["jobs", user],
+  queryFn: async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/applied/jobs/${user?.email}`);
+      console.log(res.data);
+
+      // Check if the data is undefined
+      if (res.data && res.data !== undefined) {
+        return res.data
+      } else {
+        // Handle the case where data is undefined (return a default value or handle accordingly)
+        return null; // Replace with your desired default value or appropriate handling
+      }
+    } catch (error) {
+      // Handle the error (throw or return an appropriate value)
+      console.error("Error fetching admin data:", error);
+      throw error;
+    }
+  },
+});
+
   return (
     <div>
       <div className='flex items-center gap-2 justify-center'>
         <h1 className='text-2xl font-bold'>Applied Jobs </h1>
         <FaBowlingBall className='text-purple-700 text-xl'/>
         </div>
+
+
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+       
+        <th>Job Title</th>
+        <th>Employment</th>
+        <th>Job Type</th>
+        <th>Salary</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      {
+        jobs?.map(job => <tr key={job?._id}>
+          
+          <td>
+            <div className="flex items-center gap-3">
+              
+              <div>
+                <div className="font-bold">{job?.jobTitle
+}</div>
+                <div className="text-sm opacity-50">{job?.joblocation}</div>
+              </div>
+            </div>
+          </td>
+          <td>
+            {job?.
+jobEmployment}
+          </td>
+          <td>{job?.
+jobType
+}</td>
+          <td>{job?.jobSalary
+}</td>
+          <td>{job?.
+jobLocation}</td>
+          
+          <th>
+            
+          </th>
+        </tr>)
+      }
+     
+    </tbody>
+    {/* foot */}
+    <tfoot>
+      <tr>
+      <th>Job Title</th>
+        <th>Employment</th>
+        <th>Job Type</th>
+        <th>Salary</th>
+        <th>Location</th>
+      </tr>
+    </tfoot>
+    
+  </table>
+</div>
+
+
     </div>
   )
 }
--- src/Components/Shared/Alljobs.jsx
+++ src/Components/Shared/Alljobs.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import React, { useState } from 'react'
-import { FaPeopleGroup } from "react-icons/fa6";
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
 import img from "../../../src/assets/user.gif"
 import { useForm } from 'react-hook-form';
 import { FaLocationDot } from "react-icons/fa6";
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import useAdmin from '../../Hooks/useAdmin';
+import Swal from 'sweetalert2';
 
 export const Alljobs = () => {
+  const {user} = useContext(AuthContext)
+  const [isAdmin] = useAdmin()
     const [currentPage, setCurrentPage] = useState(0);
     const [itemPerPage, setItemPerPage] = useState(10);
     const [jobtitle, setJobtitle] = useState("")
@@ -15,6 +18,13 @@ export const Alljobs = () => {
     const [salarytype,setSalarytype] = useState("")
     const [jobtype, setJobtype] = useState("")
     const [postingdate,setPostingdate]= useState("")
+
+
+    const [appliedTitle,setappliedTitle] = useState("")
+    const [appliedEmployment,setappliedEmployment] = useState("")
+    const [appliedSalary,setappliedSalary] = useState("")
+    const [appliedJobtype,setappliedJobtype] = useState("")
+    const [appliedLocation,setappliedLocation] = useState("")
 
     const { register, handleSubmit } = useForm();
 
@@ -53,6 +63,51 @@ export const Alljobs = () => {
       setSalarytype(data.salarytype)
       setJobtype(data.jobtype)
       setPostingdate(data.postingdate)
+      }
+
+      const nonApplyHandler = ()=> {
+        Swal.fire({
+          title: "Please log in to apply for this job?",
+          text: " Create an account or sign in",
+          icon: "question"
+        });
+      }
+
+      const applyHandler =(title,employment,salary,jobtype,location)=> {
+        
+        setappliedTitle(title)
+        setappliedEmployment(employment)
+        setappliedSalary(salary)
+        setappliedJobtype(jobtype)
+        setappliedLocation(location)
+        document.getElementById('my_modal_1').showModal()
+        document.getElementById('appliedbtn').addEventListener("click", ()=> {
+          document.getElementById('my_modal_1').close()
+        })
+      }
+
+      const resumehandler = async (e) => {
+        e.preventDefault()
+        const resume = e.target.resume.value 
+        const userInfo = {
+          resume :resume,
+          userEmail : user?.email,
+          jobTitle :appliedTitle,
+          jobEmployment : appliedEmployment,
+          jobSalary : appliedSalary,
+          jobType : appliedJobtype,
+          jobLocation : appliedLocation
+        }
+        
+        const res = await axios.post("http://localhost:5000/applied/jobs", userInfo)
+        if(res.data.insertedId){
+          Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+          });
+          return
+        }
       }
 
   return (
@@ -135,7 +190,7 @@ export const Alljobs = () => {
                     </div>
 
 
-                    <div className="">
+                    <div className="text-start">
                       <p>
                         <span className="text-xl font-semibold text-blue-500">
                           Employment Type:{" "}
@@ -145,7 +200,7 @@ export const Alljobs = () => {
                         </span>
                       </p>
                     </div>
-                    <div className="flex-grow">
+                    <div className="flex-grow text-start">
                       <p>
                         <span className="text-xl font-semibold text-blue-500 ">
                           Job Type:{" "}
@@ -155,18 +210,21 @@ export const Alljobs = () => {
                         </span>
                       </p>
                     </div>
-                    <div className="flex-grow">
+                    <div className="flex-grow text-start">
                       <p className="text-xl font-semibold">
                         <span className="text-blue-500 font-semibold">Salary Range:</span> $
                         {job?.salary} {job?.salaryType}
                       </p>
                     </div>
                     <div className="flex justify-between">
-                      <Link to={`/jobDetail/${job?._id}`}>
-                        <button className="btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white">
+                      {user ? <button onClick={()=> applyHandler(job?.jobtitle,job?.employment,job?.salary,job?.jobtype,job?.joblocation)} className="btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white">
                           Apply
                         </button>
-                      </Link>
+                      : <button onClick={()=>nonApplyHandler()} className="btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white">
+                      Apply
+                    </button>
+                  }
+                        
                       <div className="flex items-center gap-2">
                         <FaLocationDot className="text-2xl font-semibold" />
                         <p className='font-semibold'>{job?.joblocation}</p>
@@ -187,6 +245,29 @@ export const Alljobs = () => {
           Next
         </button>
       </div>
+
+
+<div>
+<dialog id="my_modal_1" className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Ready to take the next step?</h3>
+    <h3 className="font-bold text-lg"> Submit your CV or resume Link</h3>
+    <p className="py-4">Your application is just a click away! 🚀</p>
+    <form className="modal-action" onSubmit={resumehandler}>
+
+    <input type="text" name='resume' placeholder="Type here" className="input input-bordered w-full max-w-xs " />
+      {/* <form method="dialog"> */}
+        {/* if there is a button in form, it will close the modal */}
+        
+        <button id='appliedbtn' type='submit' className="btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white">Apply</button>
+      {/* </form> */}
+    </form>
+  </div>
+</dialog>
+</div>
+
+
+
 </div>
 
 </div>
